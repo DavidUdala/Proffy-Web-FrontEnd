f5a55bc329528ebb414065deb97d0aa2d7cb7c66
David Udala <david_udala@hotmail.com>
2020-08-07 09:38:55 -0300

    Dia 3 NLW

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,33 +2,48 @@ import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import api from '../../services/api'
 
-function TeacherItem() {
+export interface Teacher{
+    avatar: string,
+    bio:string,
+    cost:number,
+    id:number,
+    name:string,
+    subject:string,
+    whatsapp:string
+}
+
+ interface TeacherItemProps{
+    teacher: Teacher;
+}
+
+const TeacherItem:React.FC<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection() {
+        api.post('connections',{
+            user_id: teacher.id
+        });
+    }
     return (
-
         <article className="teacher-item">
             <header>
-                <img src="https://avatars3.githubusercontent.com/u/32552441?s=460&u=e91ced1ffef468fac070f389e128a542a7909a74&v=4" alt="David Udala" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>David Udala</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p>
-                Entusiasta das melhores tecnologias de química avançada.
-                <br /><br />
-                Apaixonado por explodir coisas em laboratório e por mudar a vida de pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$100,00</strong>
+                    <strong>R${teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} type="button">
                     <img src={whatsappIcon} alt="WhatsAppp" />
                     Entrar em contato.
-            </button>
+            </a>
             </footer>
         </article>
     );
